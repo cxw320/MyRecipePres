b059a937d7627d1c224ca2ee7850f509400c359b
Caroline Wang <cxw320@case.edu>
2022-09-28 14:57:08 -0400

    add singleton logic

--- app/src/main/java/com/example/myrecipepres/repository/RecipeRepository.kt
+++ app/src/main/java/com/example/myrecipepres/repository/RecipeRepository.kt
@@ -1,11 +1,12 @@
 package com.example.myrecipepres.repository
 
+import android.content.Context
 import com.example.myrecipepres.api.SpoonacularApi
 import com.example.myrecipepres.api.responsemodel.RecipeResponse
 import com.example.myrecipepres.model.Recipe
 
 
-class RecipeRepository {
+class RecipeRepository(context:Context) {
 
     suspend fun getRandomRecipes(): List<Recipe> {
          return SpoonacularApi.apiService.getRandomRecipes().body()?.recipes?.map {
@@ -19,5 +20,19 @@ class RecipeRepository {
             title = response.title,
             imageUrl = response.imageUrl
         )
+    }
+
+    companion object {
+        private var INSTANCE: RecipeRepository? = null
+
+        fun initialize(context: Context) {
+            if (INSTANCE == null) {
+                INSTANCE = RecipeRepository(context)
+            }
+        }
+
+        fun get() : RecipeRepository {
+            return INSTANCE ?: throw IllegalStateException("")
+        }
     }
 }
