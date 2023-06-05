d0d82628239dcf09436c655e87234b185fdbdaf2
Caroline Wang <cxw320@case.edu>
2023-06-05 11:08:08 -0400

    Update gradle versions

--- app/src/main/java/com/example/myrecipepres/screens/recipediscovery/RecipeDiscoveryScreen.kt
+++ app/src/main/java/com/example/myrecipepres/screens/recipediscovery/RecipeDiscoveryScreen.kt
@@ -3,8 +3,10 @@ package com.example.myrecipepres.screens.recipediscovery
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
@@ -39,7 +41,7 @@ fun RecipeDiscoveryScreen(
         LazyVerticalGrid(
             modifier = Modifier
                 .padding(5.dp),
-            cells = GridCells.Fixed(2)
+            columns = GridCells.Fixed(2)
         ) {
             items(recipeDiscoveryScreenState.recipeList) { recipe ->
                 RecipeCard(
